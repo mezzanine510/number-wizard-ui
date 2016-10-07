082fb3c967be956f574f24aab5ab225b71453484
Nahuel Ruda <mezzanine510@gmail.com>
2016-10-06 23:38:28 -0700

    Added log responses to UI button onclicks

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string name) {
+    
+        Debug.Log("Level load requested for: " + name);
+
+    }
+
+    public void QuitRequest() {
+
+        Debug.Log("I want to quit!");
+        
+    }
+
+}
